5849a7760fc20c67422421a87f8c146ee2d06863
Anton Wiklund <ixuz07@gmail.com>
2022-11-02 16:46:13 +0100

    feat: changes

---
+++ lib/comunica.ts
@@ -0,0 +1,5 @@
+import { QueryEngine } from '@comunica/query-sparql';
+
+export const fun = () => {
+  const queryEngine = new QueryEngine();
+};
---
+++ tests/unit/lib/comunica.test.ts
@@ -0,0 +1,8 @@
+import { fun } from '@/lib/comunica';
+
+describe('Comunica', () => {
+  it('tests', () => {
+    fun();
+    expect(1).toBe(2);
+  });
+});
