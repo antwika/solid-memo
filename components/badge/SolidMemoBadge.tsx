7956c43ea481438efe36612fab2f041cf1abb653
Anton Wiklund <ixuz07@gmail.com>
2022-11-09 17:43:21 +0100

    fix: remove async

--- components/badge/SolidMemoBadge.tsx
+++ components/badge/SolidMemoBadge.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import localFont from '@next/font/local'
-import { asyncComponent } from "@/lib/hack";
 import Badge from "@/ui/Badge";
 
 const myLogoFont = localFont({ src: '../../fonts/PatrickHand-Regular.ttf' })
 
-export default asyncComponent(async function SolidMemoBadge() {
+export default function SolidMemoBadge() {
   return (
     <Badge
       className={myLogoFont.className}
@@ -20,4 +19,4 @@ export default asyncComponent(async function SolidMemoBadge() {
       <span className={myLogoFont.className}>Solid Memo</span>
     </Badge>
   );
-})
+}
