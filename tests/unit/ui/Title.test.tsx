4f3e9730892f8c8f54aed0234dfc5b9ac356a8d3
Anton Wiklund <ixuz07@gmail.com>
2022-11-12 22:02:45 +0100

    fix: correct <Title> test

--- tests/unit/ui/Title.test.tsx
+++ tests/unit/ui/Title.test.tsx
@@ -1,17 +1,28 @@
 import { render, screen, within } from '@testing-library/react';
 import Title from '@/ui/Title';
 
-describe('Home', () => {
-  it('can render a title w/o icon', () => {
-    render(<Title dataTestid='test-title' text="Test title" />);
+describe('Title', () => {
+  it('can render a text w/o icon', () => {
+    render(
+      <Title
+        dataTestid='test-title'
+        text="Test title"
+      />
+    );
     expect(screen.queryByTestId('test-title')).toBeInTheDocument();
     expect(screen.queryByTestId('test-title-text')).toBeInTheDocument();
     expect(screen.queryByTestId('test-title-text')).toHaveTextContent('Test title');
     expect(screen.queryByTestId('test-title-icon')).not.toBeInTheDocument();
   });
 
-  it('can render a title w/ icon', () => {
-    render(<Title dataTestid='test-title' text="Test title" icon={<span data-testid="test-icon">Icon</span>} />);
+  it('can render a text w/ icon', () => {
+    render(
+      <Title
+        dataTestid='test-title'
+        text="Test title"
+        icon={<span data-testid="test-icon">Icon</span>}
+      />
+    );
     expect(screen.queryByTestId('test-title')).toBeInTheDocument();
     expect(screen.queryByTestId('test-title-text')).toBeInTheDocument();
     expect(screen.queryByTestId('test-title-text')).toHaveTextContent('Test title');
