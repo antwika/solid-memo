a46fb94f1ee3784678fa5e2f549c21b0e32d8f09
Anton Wiklund <ixuz07@gmail.com>
2022-11-12 23:32:09 +0100

    test: TextField typing

--- tests/unit/ui/TextField.test.tsx
+++ tests/unit/ui/TextField.test.tsx
@@ -1,13 +1,17 @@
-import { render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 import TextField from '@/ui/TextField';
 
 describe('TextField', () => {
-  it('renders', () => {
+  it('can be typed into', () => {
     render(
       <TextField
         dataTestid='test-textField'
       />
     );
     expect(screen.queryByTestId('test-textField')).toBeInTheDocument();
+    const textbox = screen.getByRole<HTMLInputElement>('textbox');
+    expect(textbox.value).toBe('');
+    fireEvent.change(textbox, { target: { value: 'Foo' }});
+    expect(textbox.value).toBe('Foo');
   });
 });
