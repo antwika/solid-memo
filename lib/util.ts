26e2085b288263050b4aee7f43bc54f85da48d66
Anton Wiklund <ixuz07@gmail.com>
2022-11-12 18:17:08 +0100

    fix: remove unused util

--- lib/util.ts
+++
@@ -1,7 +0,0 @@
-export function randomRange(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
-
-export async function wait(ms: number) {
-  return new Promise((resolve) => { setTimeout(() => { resolve(true); }, ms); });
-}
