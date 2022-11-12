f2d1f5b05af0652340ffd687e6d12f96282dba78
Anton Wiklund <ixuz07@gmail.com>
2022-11-12 16:25:53 +0100

    wip

--- components/badge/SessionBadge.tsx
+++ components/badge/SessionBadge.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import Image from "next/image";
-import localFont from '@next/font/local'
 import Badge from "@/ui/Badge";
 import useWebID from "@/hooks/useWebID";
 import { AiOutlineLock, AiOutlineUnlock } from "react-icons/ai";
-import { signIn, signOut } from "next-auth/react"
-import Button from "@/ui/Button";
-
-const myLogoFont = localFont({ src: '../../fonts/PatrickHand-Regular.ttf' })
 
 export default function SessionBadge() {
   const webID = useWebID();
