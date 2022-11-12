a3f882ac574b32badc859ad92b2e2a5d54dfad9c
Anton Wiklund <ixuz07@gmail.com>
2022-11-12 18:08:12 +0100

    fix: remove unnecessary comments

--- lib/solid.ts
+++ lib/solid.ts
@@ -48,43 +48,6 @@ export const parseResourceAsStore = async (resource: string) => {
   return new Store(parser.parse(resource));
 }
 
-/* export const extractAllCardQuadsFromStore = async (store: Store) => {
-  const quads = store.getQuads(null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://antwika.com/vocab/solidmemo/0.1/Card', null);
-  const allCardQuads = quads.reduce((acc: Record<string, Quad[]>, q) => {
-    acc[q.subject.id] = store.getQuads(q.subject.id, null, null, null);
-    return acc;
-  }, {});
-  return allCardQuads;
-}
-
-export const extractCardQuadsFromStore = async (store: Store, cardIri: string) => {
-  const cardQuads = store.getQuads(cardIri, null, null, null);
-  return cardQuads;
-}
-
-export const toCard = async (quads: Quad[]): Promise<Card> => {
-  try {
-    const store = new Store(quads);
-    console.log(store);
-    const repetition = +store.getObjects(null, 'http://antwika.com/vocab/solidmemo/0.1/repetition', null)[0].value;
-    const ease = +store.getObjects(null, 'http://antwika.com/vocab/solidmemo/0.1/ease', null)[0].value;
-    const interval = +store.getObjects(null, 'http://antwika.com/vocab/solidmemo/0.1/interval', null)[0].value;
-    const front = store.getObjects(null, 'http://antwika.com/vocab/solidmemo/0.1/front', null)[0].value;
-    const back = store.getObjects(null, 'http://antwika.com/vocab/solidmemo/0.1/back', null)[0].value;
-    return {
-      repetition,
-      ease,
-      interval,
-      front,
-      back,
-    };
-  } catch (err) {
-    const errorMessage = 'The provided quads does not conform with expected shape';
-    console.error(errorMessage, err);
-    throw new Error(errorMessage);
-  }
-} */
-
 export const addCardToStore = async (store: Store, card: Card) => {
   const temp = new Store();
   temp.addQuad(new NamedNode(card.iri), new NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), new NamedNode('http://antwika.com/vocab/solidmemo/0.1/Card'));
