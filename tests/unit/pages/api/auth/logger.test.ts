82cf21275c62a9fa8bcde237d6a3ad6ab591d792
Anton Wiklund <ixuz07@gmail.com>
2022-11-13 14:12:34 +0100

    test: next auth logger

---
+++ tests/unit/pages/api/auth/logger.test.ts
@@ -0,0 +1,21 @@
+import logger from "@/pages/api/auth/logger";
+
+describe('logger', () => {
+  it('logs info messages', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    logger.info('foo', 'bar');
+    expect(consoleSpy).toHaveBeenCalledWith('[NEXTAUTH INFO] ', 'foo', 'bar');
+  });
+
+  it('logs warning messages', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    logger.warn!('DEBUG_ENABLED');
+    expect(consoleSpy).toHaveBeenCalledWith('[NEXTAUTH WARNING] ', 'DEBUG_ENABLED');
+  });
+
+  it('logs error messages', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    logger.error!('ERROR_CODE_123', new Error('Test error'));
+    expect(consoleSpy).toHaveBeenCalledWith('[NEXTAUTH ERROR] ', new Error('Test error'), 'ERROR_CODE_123');
+  });
+});
