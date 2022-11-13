7ec9e16b286901e7c0b34818b8697f89c9d94bc5
Anton Wiklund <ixuz07@gmail.com>
2022-11-13 14:43:55 +0100

    test: more tests

---
+++ tests/unit/pages/api/auth/events.test.ts
@@ -0,0 +1,37 @@
+import events from '@/pages/api/auth/events';
+import logger from '@/pages/api/auth/logger';
+
+jest.mock('@/pages/api/auth/logger')
+
+describe('events', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('logs event upon user sign in ', () => {
+    const loggerInfoSpy = jest.spyOn(logger, 'info');
+    events.signIn!({
+      user: {
+        id: 'test-user-id',
+      },
+      account: {
+        type: 'oauth',
+        provider: 'test-provider',
+        providerAccountId: 'test-provider-id'
+      },
+    });
+    expect(loggerInfoSpy).toHaveBeenCalledTimes(1);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('User successfully signed in:', { id: 'test-user-id' }, 'provider:', 'test-provider');
+  });
+
+  it('logs event upon user sign out ', () => {
+    const loggerInfoSpy = jest.spyOn(logger, 'info');
+    events.signOut!({
+      token: {
+        webid: 'http://example.com/test/profile/card#me',
+      }
+    } as any);
+    expect(loggerInfoSpy).toHaveBeenCalledTimes(1);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Signing out user:', 'http://example.com/test/profile/card#me');
+  });
+});
---
+++ tests/unit/pages/api/auth/register.test.ts
@@ -0,0 +1,19 @@
+import * as solid from "src/lib/solid";
+import { lazyRegisterClient } from "@/pages/api/auth/register";
+
+jest.mock('src/lib/solid');
+
+describe('register', () => {
+  describe('lazyRegisterClient', () => {
+    it('registers, remembers and always return the first registered client', async () => {
+      const registerClientSpy = jest.spyOn(solid, 'registerClient');
+      registerClientSpy.mockResolvedValueOnce({ clientId: 'foo', clientSecret: 'bar' })
+      registerClientSpy.mockResolvedValueOnce({ clientId: 'never', clientSecret: 'happens' })
+      const client1 = await lazyRegisterClient('http://example.idp.com');
+      const client2 = await lazyRegisterClient('http://example.idp.com');
+      expect(client1).toStrictEqual({ clientId: 'foo', clientSecret: 'bar' });
+      expect(client2).toStrictEqual({ clientId: 'foo', clientSecret: 'bar' });
+      expect(registerClientSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
