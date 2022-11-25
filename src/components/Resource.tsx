e0a7aeea153c5e652ade298dd719d16c00483942
Anton Wiklund <ixuz07@gmail.com>
2022-11-25 04:34:33 +0100

    test: Resource

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -5,11 +5,12 @@ import {
 import Card from './Card';
 
 type Props = {
+  dataTestid?: string,
   iri: string,
   raw: string,
 };
 
-export default function Resource({ iri, raw }: Props) {
+export default function Resource({ dataTestid, iri, raw }: Props) {
   const store = new N3Store(new N3Parser().parse(raw));
   const subjects = store.getSubjects(null, null, null);
 
@@ -18,17 +19,19 @@ export default function Resource({ iri, raw }: Props) {
   subjects.forEach(subject => {
     const type = store.getObjects(subject.id, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', null)[0].value;
     if (type === 'https://antwika.com/vocab/solidmemo/0.1/Card') {
-      renderedNodes.push(<Card
-        key={subject.id}
-        iri={subject.id}
-      />);
+      renderedNodes.push(
+        <Card
+          key={subject.id}
+          iri={subject.id}
+        />
+      );
     } else {
       console.log(`Could not render resource, reason: Unknown type "${type}"`);
     }
   });
 
   return (
-    <div className="flex flex-col p-2 space-y-2">
+    <div data-testid={dataTestid} className="flex flex-col p-2 space-y-2">
       { renderedNodes }
     </div>
   );
---
+++ tests/unit/components/Resource.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen, within } from '@testing-library/react';
+import Resource from "@/components/Resource";
+
+import { renderHook, act, cleanup } from '@testing-library/react'
+import * as n3 from "n3";
+
+jest.mock('swr');
+
+var mockParse = jest.fn();
+var mockGetSubjects = jest.fn();
+var mockGetObjects = jest.fn();
+
+jest.mock('n3', () => ({
+  Parser: jest.fn(),
+  Store: jest.fn(),
+}));
+
+var Card = jest.fn((props) => (
+  <div data-testid={props.dataTestid}>{props.children}</div>
+));
+jest.mock('@/components/Card', () => (props: any) => Card(props));
+
+describe('Resource', () => {
+  beforeEach(() => {
+    const parserSpy = jest.spyOn(n3, 'Parser');
+    parserSpy.mockImplementation(() => ({ parse: mockParse }))
+
+    const storeSpy = jest.spyOn(n3, 'Store');
+    storeSpy.mockImplementation(() => ({
+      getSubjects: mockGetSubjects,
+      getObjects: mockGetObjects,
+    } as any))
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    cleanup();
+  });
+
+  it('logs a warning if the resource type is unknown', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    mockGetSubjects.mockReturnValue([{ id: 'abc' }]);
+    mockGetObjects.mockReturnValue([{ value: 'https://antwika.com/vocab/solidmemo/0.1/Null' }]);
+    render(
+      <Resource dataTestid='test-resource' iri='iri' raw='raw' />
+    );
+    
+    const resource = screen.queryByTestId('test-resource');
+    expect(resource).toBeInTheDocument();
+    expect(consoleSpy).toHaveBeenCalledWith(`Could not render resource, reason: Unknown type "https://antwika.com/vocab/solidmemo/0.1/Null"`);
+  });
+
+  it('can render a Card resource', () => {
+    mockGetSubjects.mockReturnValue([{ id: 'abc' }]);
+    mockGetObjects.mockReturnValue([{ value: 'https://antwika.com/vocab/solidmemo/0.1/Card' }]);
+    render(
+      <Resource dataTestid='test-resource' iri='iri' raw='raw' />
+    );
+    
+    const resource = screen.queryByTestId('test-resource');
+    expect(resource).toBeInTheDocument();
+  });
+});
