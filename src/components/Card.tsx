61b7d1434ea8c15f3d47cef842af021702a7a4b7
Anton Wiklund <ixuz07@gmail.com>
2022-11-25 04:16:14 +0100

    test: Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,92 +1,62 @@
 'use client'
 
-import useSWR from 'swr';
-import { sm2 } from "src/lib/sm2";
 import Badge from "src/ui/Badge";
 import Button from "src/ui/Button";
-import { useEffect, useState } from "react";
-import {
-  Parser as N3Parser,
-  Store as N3Store,
-} from 'n3';
-import { fetcher } from 'src/lib/swr';
-
-type Detail = {
-  repetition: number,
-  ease: number,
-  interval: number,
-  front: string,
-  back: string,
-}
-
-function parseDetail(iri: string, raw: string) {
-  const store = new N3Store(new N3Parser().parse(raw));
-  return {
-    repetition: +store.getObjects(iri, 'https://antwika.com/vocab/solidmemo/0.1/repetition', null)[0]?.value,
-    ease: +store.getObjects(iri, 'https://antwika.com/vocab/solidmemo/0.1/ease', null)[0]?.value,
-    interval: +store.getObjects(iri, 'https://antwika.com/vocab/solidmemo/0.1/interval', null)[0]?.value,
-    front: store.getObjects(iri, 'https://antwika.com/vocab/solidmemo/0.1/front', null)[0]?.value,
-    back: store.getObjects(iri, 'https://antwika.com/vocab/solidmemo/0.1/back', null)[0]?.value,
-  };
-}
+import { useState } from "react";
+import useCardDetail from '@/hooks/useCardDetail';
+import { sm2 } from "@/lib/sm2";
 
 type Props = {
+  dataTestid?: string,
   iri: string,
 };
 
 export default function Card(props: Props) {
-  const { data: raw } = useSWR(props.iri, fetcher);
+  const { dataTestid } = props;
+  const { cardDetail, setCardDetail } = useCardDetail(props.iri);
   const [ revealed, setRevealed ] = useState<boolean>(false);
-  const [ detail, setDetail ] = useState<Detail | undefined>();
 
-  useEffect(() => {
-    if (!raw) return;
-    setDetail(parseDetail(props.iri, raw));
-  }, [props.iri, raw]);
-
-  if (!detail) {
-    return <>Loading card...</>;
+  if (!cardDetail) {
+    return <div data-testid={dataTestid}>Loading card...</div>;
   }
 
   const onGrade = (grade: number) => {
-    const result = sm2({ grade, repetition: detail.repetition, ease: detail.ease, interval: detail.interval });
-    setDetail({
+    const result = sm2({ grade, repetition: cardDetail.repetition, ease: cardDetail.ease, interval: cardDetail.interval });
+    setCardDetail({
       repetition: result.repetition,
       ease: result.ease,
       interval: result.interval,
-      front: detail.front,
-      back: detail.back,
+      front: cardDetail.front,
+      back: cardDetail.back,
     });
   }
 
   return (
-    <div className="p-2 rounded-lg bg-slate-200 space-y-2">
+    <div data-testid={dataTestid} className="p-2 rounded-lg bg-slate-200 space-y-2">
       <Badge>IRI:{ props.iri }</Badge>
       <div className="flex space-x-2">
         <Badge>Entity:Card</Badge>
-        <Badge>Repetition:{ detail.repetition }</Badge>
-        <Badge>Ease:{ detail.ease }</Badge>
-        <Badge>Interval:{ detail.interval }</Badge>
+        <Badge>Repetition:{ cardDetail.repetition }</Badge>
+        <Badge>Ease:{ cardDetail.ease }</Badge>
+        <Badge>Interval:{ cardDetail.interval }</Badge>
       </div>
       <div className='space-x-2'>
-        <span>Front:</span><strong>{ detail.front }</strong>
+        <span>Front:</span><strong>{ cardDetail.front }</strong>
       </div>
       { revealed && (
-        <div className='space-x-2'>
-          <span>Back:</span><strong>{ detail.back }</strong>
-        </div>
+        <>
+          <div className='space-x-2'>
+            <span>Back:</span><strong>{ cardDetail.back }</strong>
+          </div>
+          <Button dataTestid={`${dataTestid}-againButton`} className="bg-red-500" onClick={() => onGrade(0)}>Again</Button>
+          <Button dataTestid={`${dataTestid}-hardButton`} className="bg-orange-500" onClick={() => onGrade(3)}>Hard</Button>
+          <Button dataTestid={`${dataTestid}-okayButton`} className="bg-green-500" onClick={() => onGrade(4)}>Okay</Button>
+          <Button dataTestid={`${dataTestid}-easyButton`} onClick={() => onGrade(5)}>Easy</Button>
+        </>
       )}
       { !revealed && (
         <>
-          <Button onClick={() => { setRevealed(true); }}>Reveal</Button>
-        </>
-      )}
-      { revealed && (
-        <>
-          <Button className="bg-red-500" onClick={() => onGrade(0)}>Again</Button>
-          <Button className="bg-orange-500" onClick={() => onGrade(3)}>Hard</Button>
-          <Button className="bg-green-500" onClick={() => onGrade(4)}>Okay</Button>
-          <Button onClick={() => onGrade(5)}>Easy</Button>
+          <Button dataTestid={`${dataTestid}-revealButton`} onClick={() => { setRevealed(true); }}>Reveal</Button>
         </>
       )}
     </div>
