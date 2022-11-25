6597f9485f318e993be5f811429cffe71da5c0f2
Anton Wiklund <ixuz07@gmail.com>
2022-11-25 02:00:43 +0100

    test: SignInOutButton

--- src/components/SignInOutButton.tsx
+++ src/components/SignInOutButton.tsx
@@ -4,12 +4,16 @@ import useWebID from "src/hooks/useWebID";
 import { signIn, signOut } from "next-auth/react"
 import Button from "src/ui/Button";
 
-export default function SignInOutButton() {
+type Props = {
+  dataTestid?: string,
+}
+
+export default function SignInOutButton({ dataTestid }: Props) {
   const webID = useWebID();
 
   if (!webID) {
-    return <Button onClick={() => signIn()}>Sign in</Button>
+    return <Button dataTestid={dataTestid} onClick={() => signIn()}>Sign in</Button>
   }
   
-  return <Button onClick={() => signOut()}>Sign out</Button>
+  return <Button dataTestid={dataTestid} onClick={() => signOut()}>Sign out</Button>
 }
---
+++ tests/unit/components/SignInOutButton.test.tsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen, within } from '@testing-library/react';
+import SignInOutButton from "@/components/SignInOutButton";
+import * as useWebID from '@/hooks/useWebID';
+import * as nextAuthReact from "next-auth/react"
+
+jest.mock('@/hooks/useWebID');
+jest.mock('next-auth/react');
+
+describe('SignInOutButton', () => {
+  let useWebIDSpy: jest.SpyInstance<any, []>;
+
+  beforeEach(() => {
+    useWebIDSpy = jest.spyOn(useWebID, 'default');
+    // let nextAuthReactSignInSpy: jest.SpyInstance<any, []>;
+    // nextAuthReactSignInSpy = jest.spyOn(NextAuthReact, 'signIn');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('renders a "Sign in" button if session is unauthenticated', () => {
+    useWebIDSpy.mockReturnValueOnce(undefined);
+    render(
+      <SignInOutButton dataTestid='test-signInOutButton' />
+    );
+    
+    const signInOutButton = screen.queryByTestId('test-signInOutButton');
+    expect(signInOutButton).toBeInTheDocument();
+    expect(signInOutButton).toHaveTextContent('Sign in');
+
+    fireEvent.click(signInOutButton!);
+    expect(nextAuthReact.signIn).toHaveBeenCalledTimes(1);
+    expect(nextAuthReact.signOut).not.toHaveBeenCalled();
+  });
+
+  it('renders a "Sign out" button if session is authenticated', () => {
+    useWebIDSpy.mockReturnValueOnce('http://example.com/test/profile/card#me');
+    render(
+      <SignInOutButton dataTestid='test-signInOutButton' />
+    );
+    
+    const signInOutButton = screen.queryByTestId('test-signInOutButton');
+    expect(signInOutButton).toBeInTheDocument();
+    expect(signInOutButton).toHaveTextContent('Sign out');
+    
+    fireEvent.click(signInOutButton!);
+    expect(nextAuthReact.signOut).toHaveBeenCalledTimes(1);
+    expect(nextAuthReact.signIn).not.toHaveBeenCalled();
+  });
+});
