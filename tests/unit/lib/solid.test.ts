2771cfbd3133bd9a9b34c400a6cbece09b9186e9
Anton Wiklund <ixuz07@gmail.com>
2022-12-01 05:22:40 +0100

    chore: remove unused code

--- tests/unit/lib/solid.test.ts
+++ tests/unit/lib/solid.test.ts
@@ -4,12 +4,30 @@ import { registerClient } from '@/lib/solid';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-jest.mock('src/lib/env', () => ({
-  getClientEnv: () => ({ NEXT_PUBLIC_BASE_URL: 'http://test.example.com' }),
-}));
-
 describe('solid', () => {
-  it('tests', async () => {
+  const originalProcessEnv = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...originalProcessEnv };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    process.env = originalProcessEnv;
+  });
+
+  it('throw an error if env variable "NEXT_PUBLIC_BASE_URL" is undefined', async () => {
+    await expect(registerClient('http://test.idp.example.com')).rejects.toThrow();
+  });
+
+  it('can register and receive a client id and secret', async () => {
+    process.env.NEXT_PUBLIC_BASE_URL = 'http://test.example.com/api/auth/callback/solid';
+
     mockedAxios.post.mockResolvedValue({ data: { client_id: 'a', client_secret: 'b' } });
     const client = await registerClient('http://test.idp.example.com');
     expect(mockedAxios.post).toHaveBeenCalledWith('http://test.idp.example.com/.oidc/reg', {
