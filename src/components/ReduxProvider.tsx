5546f74d83c57bc30c79f643f761067c49de2ddb
Anton Wiklund <ixuz07@gmail.com>
2022-12-02 05:36:40 +0100

    test: ReduxProvider

--- src/components/ReduxProvider.tsx
+++ src/components/ReduxProvider.tsx
@@ -1,9 +1,18 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../lib/store';
 
-export default function ReduxProvider({ children }: PropsWithChildren) {
-  return <Provider store={store}>{children}</Provider>;
+type Props = {
+  dataTestid?: string,
+  children: React.ReactNode,
+};
+
+export default function ReduxProvider({ dataTestid, children }: Props) {
+  return (
+    <div data-testid={dataTestid} >
+      <Provider store={store}>{children}</Provider>
+    </div>
+  );
 }
