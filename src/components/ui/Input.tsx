e03a9cec4f546f66ff76e38e189e29ccdf90c122
ixuz <ixuz07@gmail.com>
2025-04-01 23:15:31 +0200

    feat: add ui input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+export type Props = {};
+
+export default function Input({}: Props) {
+  return (
+    <input className="rounded-xl bg-white/10 p-4 text-white hover:bg-white/20" />
+  );
+}
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,3 +1,4 @@
 import Title from "@src/components/ui/Title";
 import Button from "@src/components/ui/Button";
-export { Title, Button };
+import Input from "@src/components/ui/Input";
+export { Title, Button, Input };
