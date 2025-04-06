2442cbbdb3dcd4b416f6ef4396fbe7e842d13f76
ixuz <ixuz07@gmail.com>
2025-04-06 14:55:18 +0200

    chore: remove unnecessary useSession hook

--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -1,7 +1,13 @@
-import type { Session } from "@inrupt/solid-client-authn-browser";
-import useSession from "@src/hooks/useSession";
+import {
+  getDefaultSession,
+  handleIncomingRedirect,
+  login,
+  logout,
+  type Session,
+} from "@inrupt/solid-client-authn-browser";
+import { env } from "@src/env";
 import Login from "@src/pages/login";
-import { createContext, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
 
 export const SessionContext = createContext<{
   session: Session;
@@ -18,7 +24,48 @@ type Props = {
 };
 
 export function SessionProvider({ children }: Props) {
-  const { session, tryLogIn, tryLogOut } = useSession();
+  const [session, setSession] = useState<Session>();
+
+  const tryLogIn = async (oidcIssuer: string) => {
+    await login({
+      oidcIssuer,
+      redirectUrl: new URL(
+        env.NEXT_PUBLIC_BASE_PATH,
+        window.location.href,
+      ).toString(),
+      clientName: "Solid Memo",
+    });
+  };
+
+  const tryLogOut = async () => {
+    await logout({ logoutType: "app" });
+    setSession(undefined);
+  };
+
+  const tryHandleIncomingRedirect = async () => {
+    await handleIncomingRedirect();
+    const defaultSession = getDefaultSession();
+
+    if (defaultSession.info.isLoggedIn) {
+      if (defaultSession.info.webId) {
+        console.log(`Logged in as ${defaultSession.info.webId}`);
+        setSession(defaultSession);
+      }
+      if (!defaultSession.info.webId) {
+        console.error("Missing webId!");
+      }
+    }
+  };
+
+  useEffect(() => {
+    tryHandleIncomingRedirect()
+      .then(() => {
+        console.log("Successfully handled incoming redirect (if present)");
+      })
+      .catch(() => {
+        console.log("Failed handled incoming redirect");
+      });
+  }, []);
 
   if (!session) {
     return <Login tryLogIn={tryLogIn} />;
