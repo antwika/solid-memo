14789d0ebe8a7a1d0070a460648ed6290f227035
ixuz <ixuz07@gmail.com>
2025-05-06 16:42:21 +0200

    add assessment domain model

--- packages/core/src/domain/index.ts
+++ packages/core/src/domain/index.ts
@@ -2,3 +2,4 @@ export * from "./deck.model";
 export * from "./flashcard.model";
 export * from "./instance.model";
 export * from "./schedule.model";
+export * from "./assessment.model";
---
+++ packages/core/test/unit/domain/assessment.model.test.ts
@@ -0,0 +1,23 @@
+import { parseAssessment } from "../../../src/domain/assessment.model";
+import { describe, expect, it } from "vitest";
+
+describe("assessment.model", () => {
+  describe("parseAssessment (function) (named export)", () => {
+    it("successfully parses an object that conforms to the Assessment type", () => {
+      const assessment = parseAssessment({
+        iri: "mock-iri",
+        version: "mock-version",
+        forCard: "mock-card-iri",
+        date: "2025-05-01",
+        score: 1,
+      });
+      expect(assessment).toStrictEqual({
+        iri: "mock-iri",
+        version: "mock-version",
+        forCard: "mock-card-iri",
+        date: "2025-05-01",
+        score: 1,
+      });
+    });
+  });
+});
